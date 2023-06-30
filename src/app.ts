d6c78345d5602c5d45715f71b91ec57af44f1790
kalyani <kalyani.nelluri54@gmail.com>
2023-06-30 17:18:57 +0100

    Implemented get for coffee name with static data ,Refactored app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express'; 
+import { router } from './routes/routes'; 
 export const app = express(); 
-app.get('/', (req, res) =>
- res.send('Welcome to the Drinks API!')); 
- export const app1 = express(); 
-app.get('/coffeelover', (req, res) =>
- res.send('I like coffee!'));
+app.use(express.json()); 
+app.get('/', (req, res) => res.send('Welcome to the Drinks API!')); 
+app.get('/coffeelover', (req, res) => res.send('I like coffee!')); 
+app.use('/', router); 
+
---
+++ src/controllers/coffee_controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express'; 
+// ❗ altered import 
+import * as coffeeService from '../services/coffee_service'; 
+export const getCoffee = async (req: Request<object, object, object, { coffeeName: string | undefined }>, res: Response) => { 
+const coffeeName = req.query.coffeeName; 
+const coffee = coffeeService.getCoffee(coffeeName as string); 
+res.json(coffee).status(200); 
+}; 

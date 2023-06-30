9fc54d4c3435e29fac19a04319ee696f75cb93f1
kalyani <kalyani.nelluri54@gmail.com>
2023-06-30 16:39:35 +0100

    Implement GET request for coffelover and test

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,6 @@ import express from 'express';
 export const app = express(); 
 app.get('/', (req, res) =>
  res.send('Welcome to the Drinks API!')); 
+ export const app1 = express(); 
+app.get('/coffeelover', (req, res) =>
+ res.send('I like coffee!'));

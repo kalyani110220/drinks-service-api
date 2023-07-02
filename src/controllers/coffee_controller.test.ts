e332cc59740f6bef033905a61a318bad902509b6
kalyani <kalyani.nelluri54@gmail.com>
2023-07-02 13:17:56 +0100

    additional test pass

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -7,5 +7,15 @@ expect(res.statusCode).toEqual(200);
 expect(res.body).toEqual({ 
 drinkType: 'Coffee', 
 name: 'Latte', 
-}); 
+});
+});
+test('GET /coffee with different param should return correctobject', async () => {
+const res = await request(app)
+.get('/coffee')
+.query({ coffeeName: 'Macchiato' });
+expect(res.statusCode).toEqual(200);
+expect(res.body).toEqual({
+drinkType: 'Coffee',
+name: 'Macchiato',
+});
 });
